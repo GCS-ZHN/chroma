5331ba067394625a6480adf515a8882b25dc6996
Zhang.H.N <zhang.h.n@foxmail.com>
2023-11-22 14:35:55 +0800

    chore: unconditional monomer design

---
+++ samples/unconditional_monomer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#SBATCH -J chroma_unconditional_monomer
+#SBATCH -c 8
+#SBATCH --gres=gpu:1
+#SBATCH -o chroma_unconditional_monomer_%j.log
+#SBATCH -e chroma_unconditional_monomer_%j.err
+import appdirs
+import os
+from chroma import Chroma
+from pathlib import Path
+
+out_dir = Path.cwd()
+SLURM_JOBID=os.environ.get('SLURM_JOBID', 'test')
+local_model_dir = Path(appdirs.user_cache_dir("chroma/weights"))
+chroma = Chroma(
+    weights_backbone=local_model_dir / "chroma_backbone_v1.0.pt",
+    weights_design=local_model_dir / "chroma_design_v1.0.pt"
+)
+protein = chroma.sample(chain_lengths=[200])
+protein.to(str(out_dir /f"unconditional_momomer_{SLURM_JOBID}.pdb"))
